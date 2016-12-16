d86f0f57543e96ab8ea320a8003e7cb432a9e623
Websioux <palazzi@gmail.com>
2016-12-16 14:41:51 +0100

    remove private config

--- private-config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('ADMIN_EMAIL','palazzi@gmail.com'); 
-	define('NXT_SERVER_PATH','/var/local/iota/'); 
-	define('NXT_NODE','http://localhost:14265'); 
-	define('EXTERNAL_NODE_NXT_PHP_LIB','/var/local/lib/iota-php-client'); 
-
